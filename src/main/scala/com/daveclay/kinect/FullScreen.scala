f8494a989488dcb765cf3a8b7ad75fe0144d35bf
Dave Clay <David.Clay@IGT.com>
2013-10-21 10:53:12 -0700

    added opengl library and example

--- src/main/scala/com/daveclay/kinect/FullScreen.scala
+++ src/main/scala/com/daveclay/kinect/FullScreen.scala
@@ -5,6 +5,7 @@ import processing.core.PApplet
 object FullScreen {
 
     def main(args: Array[String]) {
+        // http://www.slideshare.net/eskimoblood/processing-in-intellij
         // -Djava.libarary.path=/home/daveclay/work/processing-install/opengl.so.
         PApplet.main(Array("--present", "com.daveclay.kinect." + args(0)))
     }
