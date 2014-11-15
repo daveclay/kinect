ca261e210e504c293526af17664193641c09ed19
Dave Clay <daveclay@gmail.com>
2014-11-15 09:04:26 -0800

    weird bug with requirejs and the lift main

---
+++ src/main/webapp/js/main-lift.js
@@ -0,0 +1,43 @@
+try {
+    console.log("App.Initializing");
+
+    var initializeAppComponents = function($) {
+        require([
+            "app/Router",
+            "app/Lift"
+        ], function(Router) {
+            var router = Router.createRouter();
+            console.log("app initialized");
+        });
+    };
+
+    require.config({
+        baseUrl: 'js',
+        paths: {
+            jquery: 'vendor/jquery-2.1.1.min',
+            backbone: "vendor/backbone",
+            underscore: "vendor/underscore"
+        },
+
+        shim: {
+            'underscore': {
+                exports: '_'
+            },
+            "backbone": {
+                "deps": [ "underscore", "jquery" ],
+                "exports": "Backbone"  //attaches "Backbone" to the window object
+            }
+        }
+    });
+
+    require([
+        "jquery"
+        ], function($) {
+        console.log("jquery initialized");
+        initializeAppComponents($);
+    } );
+
+
+} catch (exception) {
+    console.log(exception);
+}
