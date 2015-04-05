823e591d5e8f8e5b0b200222fa007d0088345e76
Dave Clay <daveclay@gmail.com>
2015-04-05 10:33:46 -0700

    logging fluid velocities

--- src/main/java/com/daveclay/processing/api/image/ImgPRoc.java
+++ src/main/java/com/daveclay/processing/api/image/ImgPRoc.java
@@ -28,32 +28,37 @@ public class ImgProc {
 
     public void blur(int[] src, int[] dst, int w, int h) {
         int c;
+        int a;
         int r;
         int g;
         int b;
         for (int y = 1; y < h - 1; y++) {
             for (int x = 1; x < w - 1; x++) {
+                a = 0;
                 r = 0;
                 g = 0;
                 b = 0;
                 for (int yb = -1; yb <= 1; yb++) {
                     for (int xb = -1; xb <= 1; xb++) {
                         c = src[(x + xb) + (y - yb) * w];
+                        a += (c >> 24) & 0xFF;
                         r += (c >> 16) & 0xFF;
                         g += (c >> 8) & 0xFF;
                         b += (c) & 0xFF;
                     }
                 }
+                a /= 9;
                 r /= 9;
                 g /= 9;
                 b /= 9;
-                dst[x + y * w] = 0xFF000000 | (r << 16) | (g << 8) | b;
+                dst[x + y * w] = (a << 24) | (r << 16) | (g << 8) | b;
             }
         }
     }
 
     //you must be in RGB colorModel
     public void scaleBrightness(int[] src, int[] dst, int w, int h, float s) {
+        int a;
         int r;
         int g;
         int b;
@@ -61,10 +66,11 @@ public class ImgProc {
         for (int y = 0; y < h; y++) {
             for (int x = 0; x < w; x++) {
                 c = src[x + y * w];
+                a = (int) (s * ((c >> 24) & 0xFF));
                 r = (int) (s * ((c >> 16) & 0xFF));
                 g = (int) (s * ((c >> 8) & 0xFF));
                 b = (int) (s * ((c) & 0xFF));
-                dst[x + y * w] = 0xFF000000 | (r << 16) | (g << 8) | b;
+                dst[x + y * w] = (a << 24) | (r << 16) | (g << 8) | b;
                 //ch = hue(c);
                 //cs = saturation(c);
                 //cb = brightness(c) * s;
