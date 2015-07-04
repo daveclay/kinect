99d56d5e14490dab37554d331a79c403baa623b1
Dave Clay <daveclay@gmail.com>
2015-07-03 20:58:11 -0700

    refactor out Item

---
+++ src/main/webapp/js/main-sing.js
@@ -0,0 +1,49 @@
+try {
+    console.log("App.Initializing");
+
+    var initializeAppComponents = function($) {
+        require([
+            "app/Router",
+            "app/Sing",
+            "tweenmax"
+        ], function(Router) {
+            var router = Router.createRouter();
+            console.log("app initialized");
+        });
+    };
+
+    require.config({
+        baseUrl: 'js',
+        paths: {
+            jquery: 'vendor/jquery-2.1.1.min',
+            backbone: "vendor/backbone",
+            underscore: "vendor/underscore",
+            tweenmax: "vendor/TweenMax.min"
+        },
+
+        shim: {
+            'underscore': {
+                exports: '_'
+            },
+            backbone: {
+                deps: [ "underscore", "jquery" ],
+                exports: "Backbone"  //attaches "Backbone" to the window object
+            },
+            tweenmax: {
+                exports: "TweenMax"
+            }
+        }
+    });
+
+    require([
+        "jquery",
+        "vendor/RAF"
+        ], function($) {
+        console.log("jquery initialized");
+        initializeAppComponents($);
+    } );
+
+
+} catch (exception) {
+    console.log(exception);
+}
