df973cd8bcbff2f660c5da369a60549d0ad48bf6
Dave Clay <daveclay@gmail.com>
2015-07-13 20:19:35 -0700

    stage rects

--- src/main/java/com/daveclay/processing/sketches/StageShapeTest.java
+++ src/main/java/com/daveclay/processing/sketches/StageShapeTest.java
@@ -1,5 +1,6 @@
 package com.daveclay.processing.sketches;
 
+import com.daveclay.processing.api.Drawing;
 import com.daveclay.processing.api.SketchRunner;
 import processing.core.PApplet;
 import processing.core.PShape;
@@ -10,8 +11,6 @@ public class StageShapeTest extends PApplet {
         SketchRunner.run(new StageShapeTest());
     }
 
-    PShape s;
-
     public void setup() {
         size(1400, 768, P2D);
     }
@@ -19,46 +18,115 @@ public class StageShapeTest extends PApplet {
     public void draw() {
         background(128);
 
+        int size = 200;
+        StageRect frontLeft = new StageRect(this, StageRect.FRONT_LEFT, size);
+        StageRect frontRight = new StageRect(this, StageRect.FRONT_RIGHT, size);
+        StageRect backLeft = new StageRect(this, StageRect.BACK_LEFT, size);
+        StageRect backRight = new StageRect(this, StageRect.BACK_RIGHT, size);
+
         int circleWidth = mouseX;
-        int boxWidth = 400;
+        frontLeft.intersect(circleWidth);
+        frontRight.intersect(circleWidth);
+        backLeft.intersect(circleWidth);
+        backRight.intersect(circleWidth);
 
-        s = createShape();
-        s.beginShape();
-
-        s.vertex(0, 0);
-        s.vertex(boxWidth, 0);
-        s.vertex(boxWidth, boxWidth);
-        s.vertex(0, boxWidth);
-        s.vertex(0, 0);
-
-        s.beginContour();
-        PShape circle = createShape(ELLIPSE, boxWidth - circleWidth / 2, boxWidth - circleWidth / 2, circleWidth, circleWidth);
-        //PShape circle = createShape(RECT, 0, 0, 80, 80);
-        for (int i = 1; i < circle.getVertexCount(); i++) {
-            PVector vertex = circle.getVertex(i);
-            float x = vertex.x;
-            float y = vertex.y;
-            if (x > boxWidth) {
-                x = boxWidth;
-            }
-            if (x < 0) {
-                x = 0;
-            }
-            if (y < 0) {
-                y = 0;
-            }
-            if (y > boxWidth) {
-                y = boxWidth;
-            }
-            s.vertex(x, y);
-        }
-        s.endContour();
-
-// Finishing off shape
-        s.endShape();
-
+        pushMatrix();
         noStroke();
-        fill(255, 0, 0);
-        shape(s, 100, 100);
+        fill(255, 0, 0, 80);
+        frontLeft.draw();
+
+        fill(0, 255, 0, 80);
+        translate(size, 0);
+        frontRight.draw();
+
+        fill(0, 0, 255, 80);
+        translate(-size, size);
+        backLeft.draw();
+
+        fill(255, 0, 255, 80);
+        translate(size, 0);
+        backRight.draw();
+        popMatrix();
+
+        fill(0, 255, 255, 80);
+        ellipse(size, size, circleWidth, circleWidth);
+    }
+
+    static class StageRect extends Drawing {
+        public static final int FRONT_LEFT = 1;
+        public static final int FRONT_RIGHT = 2;
+        public static final int BACK_LEFT = 3;
+        public static final int BACK_RIGHT = 4;
+
+        private float size;
+        private float circleWidth;
+        private int corner = FRONT_LEFT;
+        private PShape shape;
+
+        public StageRect(PApplet canvas, int corner, float size) {
+            super(canvas);
+            this.corner = corner;
+            this.size = size;
+        }
+
+        public void intersect(float circleWidth) {
+            this.circleWidth = circleWidth;
+        }
+
+        public void draw() {
+            shape = createShape();
+            shape.beginShape();
+
+            shape.vertex(0, 0);
+            shape.vertex(size, 0);
+            shape.vertex(size, size);
+            shape.vertex(0, size);
+            shape.vertex(0, 0);
+
+            contour();
+
+            shape.endShape();
+
+            shape(shape, 0, 0);
+        }
+
+        private void contour() {
+            shape.beginContour();
+
+            float circleX, circleY;
+            if (corner == FRONT_LEFT) {
+                circleX = size - circleWidth / 2;
+                circleY = circleX;
+            } else if (corner == BACK_RIGHT) {
+                circleX = -1 * (circleWidth / 2);
+                circleY = circleX;
+            } else if (corner == BACK_LEFT) {
+                circleX = size - circleWidth / 2;
+                circleY = -1 * (circleWidth / 2);
+            } else {
+                circleY = size - circleWidth / 2;
+                circleX = -1 * (circleWidth / 2);
+            }
+            PShape circle = createShape(ELLIPSE, circleX, circleY, circleWidth, circleWidth);
+            for (int i = 1; i < circle.getVertexCount(); i++) {
+                PVector vertex = circle.getVertex(i);
+                float x = vertex.x;
+                float y = vertex.y;
+                if (x > size) {
+                    x = size;
+                }
+                if (x < 0) {
+                    x = 0;
+                }
+                if (y < 0) {
+                    y = 0;
+                }
+                if (y > size) {
+                    y = size;
+                }
+                shape.vertex(x, y);
+            }
+            shape.endContour();
+        }
     }
 }
