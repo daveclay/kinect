c64f99e1aa98f6957ae0ae7e7e99c38068ad5046
Dave Clay <daveclay@gmail.com>
2015-07-18 10:25:44 -0400

    Twerking

--- src/main/java/com/daveclay/processing/openprocessing/deJong/deJong.java
+++ src/main/java/com/daveclay/processing/openprocessing/deJong/deJong.java
@@ -33,36 +33,31 @@ public class deJong extends PApplet {
     public void draw() {
         if (!stop) {
             stepCounter++;
-            if (stepCounter > 127) {
+            if (stepCounter > 327) {
                 stop = true;
                 return;
             }
             dj.incrementalupdate();
+            image(dj.pi, 0, 0, width, height);
         }
-        image(dj.pi, 0, 0, width, height);
     }
 
     public void mouseDragged() {
-        noLoop();
-        stop = true;
         dj.reparam();
-        redraw();
     }
 
     public void mouseReleased() {
-        loop();
-        stop = false;
         stepCounter = 0;
-        dj.updateloop();
+        stop = false;
     }
 
     class deJongAttractor {
         PImage pi;
         float pa, pb, pc, pd, newx, newy, oldx, oldy, logmaxd;
-        int N = width;
+        int IMAGE_SIZE = width;
         int maxdense = 0;
-        int[][] density = new int[N][N];
-        float[][] previousx = new float[N][N];
+        int[][] density = new int[IMAGE_SIZE][IMAGE_SIZE];
+        float[][] previousx = new float[IMAGE_SIZE][IMAGE_SIZE];
 
         void construct() {
             //Produces the four variables to pass to the attractor
@@ -75,13 +70,11 @@ public class deJong extends PApplet {
             oldy = height/2;
         }
 
-        void populate(int s, boolean c) {
+        void populate(int samples, boolean clear) {
             //Populate array with density info with s number of samples
-            int samples = s;
-            boolean clear = c;
             if (clear) {
-                for (int i = 0; i < N; i++) {
-                    for (int j = 0; j < N; j++) {
+                for (int i = 0; i < IMAGE_SIZE; i++) {
+                    for (int j = 0; j < IMAGE_SIZE; j++) {
                         density[i][j] = 0;
                         previousx[i][j] = 0;
                     }
@@ -101,15 +94,15 @@ public class deJong extends PApplet {
                          n+1 = sin(a yn) - cos(b xn)
                          yn+1 = sin(c xn) - cos(d yn)
                      */
-                    newx = (float) (((sin(pa * oldy) - cos(pb * oldx)) * N * 0.2) + N/2);
-                    newy = (float) (((sin(pc * oldx) - cos(pd * oldy)) * N * 0.2) + N/2);
+                    newx = (float) (((sin(pa * oldy) - cos(pb * oldx)) * IMAGE_SIZE * 0.2) + IMAGE_SIZE /2);
+                    newy = (float) (((sin(pc * oldx) - cos(pd * oldy)) * IMAGE_SIZE * 0.2) + IMAGE_SIZE /2);
 
 
                     //Smoothie
                     newx += random(-0.001f, 0.001f);
                     newy += random(-0.001f, 0.001f);
                     //If coordinates are within range, up density count at its position
-                    if ((newx > 0) && (newx < N) && (newy > 0) && (newy < N) ) {
+                    if ((newx > 0) && (newx < IMAGE_SIZE) && (newy > 0) && (newy < IMAGE_SIZE) ) {
                         density[(int)newx][(int)newy] += 1;
                         previousx[(int)newx][(int)newy] = oldx;
                     }
@@ -118,8 +111,8 @@ public class deJong extends PApplet {
                 }
             }
             //Put maximum density and its log()-value into variables
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
+            for (int i = 0; i < IMAGE_SIZE; i++) {
+                for (int j = 0; j < IMAGE_SIZE; j++) {
                     if (density[i][j] > maxdense) {
                         maxdense = density[i][j];
                         logmaxd = log(maxdense);
@@ -128,15 +121,10 @@ public class deJong extends PApplet {
             }
         }
 
-        void updateloop() {
-            stop = false;
-            stepCounter = 0;
-        }
-
         void incrementalupdate() {
             //Loops the non-clearing update and plotting to produce low-noise render
             populate(16, false);
-            plot(0, false);
+            plot(20, false);
             redraw();
         }
 
@@ -147,24 +135,22 @@ public class deJong extends PApplet {
             dj.plot(100, true);
         }
 
-        PImage plot(int f, boolean c) {
-            int factor = f;
-            boolean clear = c;
+        PImage plot(int factor, boolean clear) {
             //Plot image from density array
             if (clear) {
-                pi = createImage(N, N, RGB);
+                pi = createImage(IMAGE_SIZE, IMAGE_SIZE, RGB);
             }
             pi.loadPixels();
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
+            for (int i = 0; i < IMAGE_SIZE; i++) {
+                for (int j = 0; j < IMAGE_SIZE; j++) {
                     if (density[i][j] > 0) {
-                        float myhue = map(previousx[i][j], 0, N, 128, 255); //Select hue based on the x-coord that gave rise to current coord
+                        float myhue = map(previousx[i][j], 0, IMAGE_SIZE, 128, 255); //Select hue based on the x-coord that gave rise to current coord
                         float mysat = map(log(density[i][j]), 0, logmaxd, 128, 0);
                         float mybright = map(log(density[i][j]), 0, logmaxd, 0, 255) + factor;
                         int newc = color(myhue, mysat, mybright);
-                        int oldc = pi.pixels[i * N + j];
+                        int oldc = pi.pixels[i * IMAGE_SIZE + j];
                         newc = blendColor(newc, oldc, SOFT_LIGHT);
-                        pi.pixels[i * N + j] = newc;
+                        pi.pixels[i * IMAGE_SIZE + j] = newc;
                     }
                 }
             }
